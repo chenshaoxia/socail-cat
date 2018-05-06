aa8f713989fe02d6864071857a93e9cd03807a1b
Kant <2777187968@qq.com>
2018-05-06 12:08:21 +0800

    整理文件
    删除了不必要的文件，将dist目录移到最上一级

--- jiyi/src/main/webapp/js/button.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.skill-edit-input-pic input').change(function () {
-        var arrs=$(this).val().split('\\');
-        var filename=arrs[arrs.length-1];
-        $(".skill-edit-input-content").html(filename);
-    });
-});
